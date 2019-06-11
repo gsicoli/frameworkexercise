7ab35cc2571bb6d32beb7b02f2c3fc89bb8d23fa
gsicoli <gsicoli@daitangroup.com>
2019-06-11 09:54:28 -0300

    Environment configuration

---
+++ .eslintrc.js
@@ -0,0 +1,44 @@
+module.exports = {
+    env: {
+      es6: true,
+      mocha: true,
+    },
+    extends: [
+      'airbnb-base',
+    ],
+    plugins: [
+      'import',
+      'async-await',
+      'promise'
+    ],
+    globals: {
+      GIT_COMMIT: true,
+      GIT_BRANCH: true,
+    },
+    settings: {
+    },
+    parserOptions: {
+    },
+    rules: {
+      'max-len': ['error', {
+        code: 160,
+      }],
+      'no-plusplus': ['off'],
+      'prefer-template': 0,
+      'global-require': 0,
+      'object-curly-newline': ['off'],
+      'prefer-promise-reject-errors': 0,
+      'dot-notation': [
+        'error',
+        {
+          allowPattern: '^[A-Z]+(_[A-Z]+)+$',
+        },
+      ],
+      'no-underscore-dangle': [
+        'error',
+        {
+          allowAfterThis: true
+        },
+      ],
+    },
+  };
