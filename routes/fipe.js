0101e4f2f81406dd49430b11a76e42f74c69d3e8
gsicoli <gsicoli@daitangroup.com>
2019-06-13 09:42:56 -0300

    Adding route to get 'tipos de veiculo' and fix on await position.

--- routes/fipe.js
+++ routes/fipe.js
@@ -1,52 +1,46 @@
 const rp = require('request-promise');
 const router = require('express').Router();
 
-let tipos = ['carros', 'motos', 'caminhoes'];
-let marcasDisponiveis = {};
-let veiculosDisponiveis = {};
-let modelosDisponiveis = {};
+const tipos = ['carros', 'motos', 'caminhoes'];
+let marcasDisponiveis = [];
+let veiculosDisponiveis = [];
+let modelosDisponiveis = [];
 let veiculo = {};
 
-const makeRequest = async (path) => {
-  let result;
+const makeRequest = (path) => {
   const options = {
     uri: 'https://fipeapi.appspot.com/api/1/' + path,
     method: 'GET',
     json: true,
   };
-  try {
-    result = await rp(options);
-    console.log(result);
-  } catch (err) {
-    console.log(err);
-  }
-  return result;
+  return rp(options);
 };
 
-const fetchMarcas = (req, res, next) => {
+const fetchMarcas = async (req, res, next) => {
   const { tipo } = req.query;
-  marcasDisponiveis = makeRequest(`${tipo}/marcas.json`);
+  marcasDisponiveis = await makeRequest(`${tipo}/marcas.json`);
   next();
 };
 
 const fetchVeiculos = async (req, res, next) => {
   const { tipo, id } = req.query;
-  veiculosDisponiveis = makeRequest(`${tipo}/veiculos/${id}.json`);
+  veiculosDisponiveis = await makeRequest(`${tipo}/veiculos/${id}.json`);
   next();
 };
 
 const fetchModelos = async (req, res, next) => {
   const { tipo, id, idModelo } = req.query;
-  modelosDisponiveis = makeRequest(`${tipo}/veiculo/${id}/${idModelo}.json`);
+  modelosDisponiveis = await makeRequest(`${tipo}/veiculo/${id}/${idModelo}.json`);
   next();
 };
 
 const fetchVeiculo = async (req, res, next) => {
   const { tipo, id, idModelo, idAno } = req.query;
-  veiculo = makeRequest(`${tipo}/veiculo/${id}/${idModelo}/${idAno}.json`);
+  veiculo = await makeRequest(`${tipo}/veiculo/${id}/${idModelo}/${idAno}.json`);
   next();
 };
 
+router.get('/tipos', (req, res) => res.send(tipos));
 router.get('/marcas', fetchMarcas, (req, res) => res.send(marcasDisponiveis));
 router.get('/veiculos', fetchVeiculos, (req, res) => res.send(veiculosDisponiveis));
 router.get('/modelos', fetchModelos, (req, res) => res.send(modelosDisponiveis));
