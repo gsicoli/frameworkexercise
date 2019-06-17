d168f2143335ebfc9e43bde828096a6f5e376512
gsicoli <gsicoli@daitangroup.com>
2019-06-17 09:33:09 -0300

    Adding next for server side rendering of react. File 'next.config.js' allows CSS and SASS to work properly.

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const next = require('next');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const fipe = require('./routes/fipe');
@@ -16,7 +17,18 @@ app.use('/fipe', fipe);
 app.use('/db-access', dbOps);
 
 const startServer = async () => {
-  await connectDb();
+  const isDev = process.env.NODE_ENV !== 'production';
+  const nextRenderer = next({ isDev });
+  const nextHandler = nextRenderer.getRequestHandler();
+
+  try {
+    await Promise.all([nextRenderer.prepare(), connectDb()]);
+  } catch (err) {
+    console.log(err);
+    return;
+  }
+
+  app.get('*', (req, res) => nextHandler(req, res));
 
   app.listen(process.env.SERVER_PORT, () => {
     console.log('Server is running.');
---
+++ pages/index.js
@@ -0,0 +1,3 @@
+export default () => (
+  <div><p>Essa página é renderizada pelo o Next.</p></div>
+);
