430d9e3dae553a36b431f7a280333dbb1dc73c2a
gsicoli <gsicoli@daitangroup.com>
2019-06-20 15:58:36 -0300

    Page for search is now named in English.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import Link from 'next/link';
 const Navbar = () => (
   <div className='topnav'>
     <Link href="/"><a>Home</a></Link>
-    <Link href="/consulta"><a>Nova Consulta</a></Link>
+    <Link href="/search"><a>Nova Consulta</a></Link>
     <Link href="/salvos"><a>Consultas Salvas</a></Link>
   </div>
 );
